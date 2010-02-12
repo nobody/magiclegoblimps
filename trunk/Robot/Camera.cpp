b8ab521180501c5fb19910bb8023eb1123d9ce08
eXceLynX <eXceLynX@445d4ad4-0937-11df-b996-818f58f34e26>
2010-02-12 01:19:49 +0000

    Uploading initial robot framework.

---
+++ trunk/Robot/Camera.cpp
@@ -0,0 +1,69 @@
+#include "Camera.h"
+
+Camera::Camera(string ip, bool dLinkCam)
+{
+	ip_ = ip;
+	dLinkCam_ = dLinkCam;
+}
+
+bool Camera::Connect()
+{
+	capture_ = 0;
+
+	dLinkUrl_ = "/video.cgi?a=.mjpg";
+	ciscoUrl_ = "/img/mjpeg.cgi?a=.mjpg";
+
+	user_ = "admin";
+	pass_ = "admin";
+
+	string camUrl;
+
+	camUrl = "http://" + user_ + ":" + pass_ + "@" + ip_;
+
+	if (dLinkCam_)
+		camUrl += dLinkUrl_;
+	else
+		camUrl += ciscoUrl_;
+
+	capture_ = cvCreateFileCapture(camUrl.c_str());
+
+	if (capture_ == 0)
+		return false;
+
+	return true;
+}
+
+void Camera::SetIP(string ip)
+{
+	ip_ = ip;
+}
+
+void Camera::SetDLinkCam(bool dLinkCam)
+{
+	dLinkCam_ = dLinkCam;
+}
+
+void Camera::StartDisplay()
+{
+	cvNamedWindow("TestWindow", CV_WINDOW_AUTOSIZE);
+}
+
+void Camera::DisplayFrame()
+{
+	IplImage* frame = cvQueryFrame(capture_);
+
+	if(!frame) 
+	{
+		fprintf(stderr, "ERROR: Frame is NULL! \n");
+		getchar();
+		return;
+	}		
+
+	cvShowImage("TestWindow", frame);
+}
+
+void Camera::StopDisplay()
+{
+	cvReleaseCapture(&capture_);
+	cvDestroyWindow("TestWindow");
+}
