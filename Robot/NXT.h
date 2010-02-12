446942456d08c1b2f306424812d81493d5f31a15
eXceLynX <eXceLynX@445d4ad4-0937-11df-b996-818f58f34e26>
2010-02-12 01:19:07 +0000

    Uploading initial robot framework.

---
+++ Robot/NXT.h
@@ -0,0 +1,53 @@
+#ifndef NXT_H
+#define NXT_H
+
+#include <string>
+
+#include "NXTBluetooth/include/nxt.h"
+
+#pragma comment(lib, "NXTBluetooth/lib/NXTBluetooth.lib")
+
+class NXT
+{
+public:
+	NXT(int port);
+
+	bool Connect();
+	void Disconnect();
+	
+	void Drive();
+	void TurnLeft();
+	void TurnRight();
+	void Stop();
+
+	void FollowLine();
+
+	void PanLeft();
+	void PanRight();
+
+	void SetPort(int port);
+
+	int GetBatteryLevel();
+	void SendMessage(string message, int box);
+	string GetMessage(int box, bool remove);
+	void StartProgram(string name);
+	void StopPrograms();
+
+
+private:
+	static const int DRIVE_SPEED = 20;
+	static const int TURN_SPEED = 30;
+	static const int PAN_SPEED = 10;
+
+	int bluetoothPort_;
+
+	Connection* connection_;
+	Brick* brick_;
+	Motor* motorA_;
+	Motor* motorB_;
+	Motor* motorC_;
+	Sensor* lSensorL_;
+	Sensor* lSensorR_;
+};
+
+#endif
