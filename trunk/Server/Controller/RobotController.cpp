6557ba52cc27f8dffd85b97bcaf0931f474d6564
JoeGebhard <JoeGebhard@445d4ad4-0937-11df-b996-818f58f34e26>
2010-02-23 16:36:28 +0000

    Completed code to accept a simple socket some is commented out because I don't know if it will compile

--- trunk/Server/Controller/RobotController.cpp
+++ trunk/Server/Controller/RobotController.cpp
@@ -7,6 +7,7 @@
  */
 
 #include "RobotController.h"
+#include <iostream>
 
 RobotController::RobotController(boost::asio::io_service& io_service) {
     ioService = io_service;
@@ -19,11 +20,13 @@ RobotController::~RobotController() {
 
 //this method listens on a socket for connections
 RobotController::void listen(){
-
+   boost::asio::ip::tcp::socket socket_(ioService);
+   boost::asio::ip::tcp::acceptor acceptor_(ioService, boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), 5060));
+  // acceptor_.async_accept(socket_, &RobotController::onconnect());
 }
 //this method handles intial connections to the server
 RobotController::void onconnect(){
-
+   std::cout<<"connection accepted";
 }
 
 /* vi: set tabstop=4 expandtab shiftwidth=4 softtabstop=4: */
