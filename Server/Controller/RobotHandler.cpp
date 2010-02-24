9e72714521d688ac39d6b4c7faf28d55f52ad9be
JoeGebhard <JoeGebhard@445d4ad4-0937-11df-b996-818f58f34e26>
2010-02-24 23:28:32 +0000

    [server] Created a threaded Handler class for the robot server and added it to the make file

---
+++ Server/Controller/RobotHandler.cpp
@@ -0,0 +1,33 @@
+/* AdminHandler.cpp
+ *
+ * Modified on:    $Date$
+ * Last Edited By: $Author$
+ * Revision:       $Revision$
+ */
+
+
+#include "RobotHandler.h"
+
+conn_map RobotHandler::connections;
+
+RobotHandler::RobotHandler(){}
+RobotHandler::~RobotHandler(){}
+
+void RobotHandler::onConnect(TcpServer::TcpConnection::pointer tcp_connection){
+
+    //enter the tcp connection pointer into the map
+    connections[tcp_connection->socket().remote_endpoint()] = tcp_connection;
+
+    //insert initialization stuff here
+    
+    //spawn a thread to listen on the socket and return the function
+    boost::thread connThread(&RobotHandler::threaded_listen, this, tcp_connection->socket().remote_endpoint());
+}
+
+void RobotHandler::threaded_listen(const boost::asio::ip::tcp::endpoint connEP){
+        std::string msg = "your robot socket is being handled";
+        boost::asio::write(connections[connEP]->socket(), boost::asio::buffer(msg));
+        connections[connEP]->socket().close();
+}
+
+/*vi: set tabstop=4 expandtab shiftwidth=4 softtabstop=4:*/
