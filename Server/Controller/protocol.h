8765de722ab7b264129b2ffc9544e1a67c3f8f12
JoeGebhard <JoeGebhard@445d4ad4-0937-11df-b996-818f58f34e26>
2010-03-24 20:52:59 +0000

    [server] added protocol.h to convert certain types of data to char arrays

---
+++ Server/Controller/protocol.h
@@ -0,0 +1,150 @@
+/*
+ * protocol.h 
+ *
+ * Modified on:    $Date$
+ * Last Edited By: $Author$
+ * Revision:       $Revision$
+ */
+
+#include <cstring>
+
+#define P_ROBOT_INIT 1
+#define P_ROBOT_UPDATE 2
+#define P_OBJECT 3
+
+/* the protocol converts arrays of the defined structs into byte arrays 
+ * from transmission over the socket the protocol is pretty much as 
+ * follows, the first byte if the type of struct in the message,
+ * the second and third bytes are short representing the total
+ * number of structs transmitted. Each struct is prefixed by a two byte
+ * short listing the size of that particular struct instance followed by
+ * the struct itself. after that each type of struct is unique.
+ */
+
+struct robotInit{
+    int RID;
+    int x;
+    int y;
+    std::string* VideoURL;
+};
+struct robotUpdate{
+    int x;
+    int y;
+};
+struct object{
+    int OID;
+    std::string* name;
+    char* color;
+    size_t color_size;
+};
+
+struct byteArray{
+    char* array;
+    int size;
+};
+
+void write(robotInit* data, short number, byteArray* byte_ptr){
+    char* array;
+    unsigned short* sizes = new unsigned short[number];
+    int overall_size = 0;
+    char** structs = new char*[number];
+    for(int i = 0; i < number; ++i){
+        short str_len = data[i].VideoURL->size();
+        char* url = data[i].VideoURL->c_str;
+        short size = sizeof(int)*3 + str_len + 2;
+        structs[i] = new char[size];
+
+        //put the size on the top of the array
+        char* ref = &size;
+        for(int j = 0; j < 2; ++j){
+            structs[i][j] = ref[j];
+        }
+
+        //push rid on the array
+        ref = &(data[i].RID);
+        for(int j = 2; j < 6; ++j){
+            structs[i][j] = ref[j-2];
+        }
+
+        //push x
+        ref = &(data[i].x);
+        for(j = 6; j <10; ++j){
+            structs[i][j] = ref[j-6];
+        }
+
+        //push y
+        ref = &(data[i].y);
+        for(j = 10; j < 14; ++j){
+            structs[i][j] = ref[j-10];
+        }
+
+        //push the string size
+        ref = &str_len;
+        for(j = 14; j < 16; ++j){
+            structs[i][j] = ref[j-14];
+        }
+
+        //push the string
+        for(j = 16; j < 16+str_len; ++j){
+            structs[i][j] = ref[j - 16];
+        }
+
+        //add the length of this array to the total length and store it
+        sizes[i] = size;
+        overall_size += size;
+        delete[] url;
+    }
+
+    //intiallize the array to return
+    array = new char[overall_size + 3];
+
+    //set type
+    array[0] = P_ROBOT_INIT;
+
+    //push number of elements;
+    char* ref = &number;
+    for(int i = 1; i < 3; ++i){
+        array[i] = ref[i -1];
+    }
+    
+    //push each array onto the array
+    int position = 3;
+    for(int i = 0; i < number; ++i){
+             memcpy(&array[position], structs[i], sizes[i]);
+             position += sizes[i];
+    }
+    //clean up
+    delete[] sizes;
+    for(int i = 0; i < number; ++i){
+        delete[] structs[i];
+    }
+    delete structs;
+    byte_ptr->array = array;
+    byte_ptr->size = overall_size + 3;
+
+}
+
+
+void write(robotUpdate* data, short number, byteArray* byte_ptr){
+    char* array;
+    unsigned short* sizes = new unsigned short[number]; 
+    int overall_size = 0;
+    char** structs = new char*[number];
+    //for each struct
+    for(int i = 0; i < number; ++i){
+        short size = sizeof(int) * 2;
+        structs[i] = new char[size];
+
+        //push size;
+        memcpy(structs[i], &size, sizeof(short));
+
+        //push x
+        
+
+    }
+}
+int read(){
+
+}
+
+/* vi: set tabstop=4 expandtab shiftwidth=4 softtabstop=4: */
