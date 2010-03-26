86a4be6d3065333126a0e6bdee00f3d1a2771760
jordanbach <jordanbach@445d4ad4-0937-11df-b996-818f58f34e26>
2010-03-26 03:10:35 +0000

    Updates to the video server scripts - not tested yet

--- Server/VideoDelivery/ffserver.py
+++ Server/VideoDelivery/ffserver.py
@@ -1,41 +1,27 @@
 import subprocess
 
-class Manager():
+def launch(vfeed):
     """
-    Manages ffserver processes on the video delivery server.
+    Launch a new ffserver/ffmpeg live feed
     """
-    
-    def __init__(self):
-        self.ffserver_procs = []
-        self.ffmpeg_procs = []
-        self.reserved_ports = {}
-        self.config_files = {}
-    
-    def launchInstance(self, infile, outfile, inconf=None, outconf=None):
-        print("launch")
-        # determine ports/files/urls to use
-        # create a new config file with correct parameters
-        # start a new ffserver instance
-        # start a new ffmpeg instance
-        # detach these processes
-        args = ['ffmpeg']
-        if inconf is not None:
-            for k, v in inconf.items():
-                args.append(k)
-                args.append(v)
-        args.append('-i')
-        args.append(infile)
-        if outconf is not None:
-            for k, v in outconf.items():
-                args.append(k)
-                args.append(v)
-        args.append(outfile)
-        
-        devnull = open('/dev/null', 'w').fileno()
-        ffmpeg_proc = subprocess.Popen(args)
-        print("launched")
-        pass
-    
-    def killInstance(self, pid1, pid2):
-        print("kill")
-        pass
+    pass
+
+def kill(vfeed):
+    """
+    Kill an existing ffserver/ffmpeg live feed
+    """
+    pass
+
+def ffserver_args(vfeed):
+    return ('ffserver', '-f', vfeed.config_file)
+
+def ffmpeg_args(vfeed):
+    return (
+        'ffmpeg',
+        '-r', '15',
+        '-b', '1024k',
+        '-i', vfeed.feed_url,
+        settings.LIVE_FEED_URLS.format(settings.CURRENT_IP, vfeed.port,
+                                       vfeed.feed_name)
+    )
+
