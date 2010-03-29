e9507ac4f5c65617756abd158c4c738942513cfa
JoeGebhard <JoeGebhard@445d4ad4-0937-11df-b996-818f58f34e26>
2010-03-29 21:36:41 +0000

    [server] robot handler now sends objects when controllers connect, cleaned up robot handler

--- Server/Controller/RobotHandler.cpp
+++ Server/Controller/RobotHandler.cpp
@@ -43,13 +43,14 @@ void RobotHandler::onConnect(TcpServer::TcpConnection::pointer tcp_connection){
 	if(error == boost::asio::error::eof){
 		std::cout<<"EOF\n";
         //die gracefully, think i can do it by calling the cleanup connection method
-        
+        	cleanupConn(connEP);
         return;
 		
 	}
 	if(error == boost::asio::error::operation_aborted){
 		std::cout<<"aborted..\n";
 		//die gracefully
+		cleanupConn(connEP);
 
         return;
 	}
@@ -83,13 +84,13 @@ void RobotHandler::onConnect(TcpServer::TcpConnection::pointer tcp_connection){
 		if(error == boost::asio::error::eof){
 			std::cout<<"EOF\n";
 			//die gracefully;
-
+			cleanupConn(connEP);
 			return;
 		}
 		if(error == boost::asio::error::operation_aborted){
 			std::cout<<"aborted..\n";
-            //die gracefully 
-
+            		//die gracefully 
+			cleanupConn(connEP);
 			return;
 		}
 	}
@@ -119,21 +120,22 @@ void RobotHandler::onConnect(TcpServer::TcpConnection::pointer tcp_connection){
 
 	//switch on the type of struct recovered
 	if(message->type != P_ROBOT_INIT){
-        //die gracefully
-    }	
+        	//die gracefully
+		cleanupConn(connEP);
+   	 }	
 	
-    //initilize stuff
+   	//initilize stuff
 	Vector_ts<Robot*>::iterator it;
 	robotInit* robotData = new robotInit[message->size];
 	robotData = (robotInit*)(message->array);
 
-    //get a lock on the vector
+   	//get a lock on the vector
 	robots->lock();
 	for(int i = 0; i < message->size; ++i){
 		Robot* temp = new Robot(connEP, robotData[i].RID);
-        temp->setXCord(robotData[i].x);
-        temp->setYCord(robotData[i].y);
-        //temp->setVideoURL(std::string(robotData[i].VideoURL));
+       		 temp->setXCord(robotData[i].x);
+        	 temp->setYCord(robotData[i].y);
+        	 temp->setVideoURL(std::string(*robotData[i].VideoURL));
         
         robots->push_back(temp);
         //might want to clean up some stuff here if patrick doesn't fix destructor
@@ -165,23 +167,14 @@ void RobotHandler::onConnect(TcpServer::TcpConnection::pointer tcp_connection){
 	byteArray* byte_ptr = new byteArray;
 	write_data(P_OBJECT, objArr, objects->size(), byte_ptr);
 	objects->readUnlock();
-
-	//not sure that anything past here is 
-	//need to put the array in a buffer
-	boost::asio::streambuf outputBuffer;	
-	/*data = outputBuffer.prepare(byte_ptr->size);
-	iter = boost::asio::buffers_begin(data);
-
-	//need some kind of for loop to put the array into the buffer;
-	for(int i = 0; i < byte_ptr->size; ++i){
-		*iter = byte_ptr->array[i];
-		iter++;
-	}*/
-
+	delete[] objArr;	
+	
 	//hopefully the number of bytes is always a positive number
-	boost::asio::write(connections[connEP]->socket(), outputBuffer, boost::asio::transfer_at_least(byte_ptr->size), error);
+	boost::asio::write(connections[connEP]->socket(),  boost::asio::buffer(byte_ptr->array, byte_ptr->size),
+		boost::asio::transfer_at_least(byte_ptr->size), error);
 
-    
+   	connections[connEP]->releaseSocket();
+	delete byte_ptr;
     //spawn a thread to listen on the socket and return the function
     std::cout<<"launching threadd...\n";
     boost::thread connThread(&RobotHandler::threaded_listen, this, connEP);
@@ -338,106 +331,6 @@ void RobotHandler::threaded_listen(const boost::asio::ip::tcp::endpoint connEP){
 //clean up stuff from connection
 }
 
-void RobotHandler::parseRobots(std::string* msg, boost::asio::ip::tcp::endpoint connEP, int use){
-
-bool moreRobots = true;
-size_t end = 0;
-int robotID = 0;
-int x = 0; 
-int y = 0;
-std::string current;
-while(moreRobots){
-
-//parse robot data from string here
-//find the end of the id
-end = msg->find('$');
-
-//check to see if there is a dollar sign
-if (end != std::string::npos){
-	//pull out the id and create a temporary string that is the 
-	//current string minus the id, delete the current string and 
-	//reassign the pointer to the new string;
-	current = msg->substr(0, end);
-	std::string* temp = new std::string(msg->substr(end+1, std::string::npos));
-	delete msg;
-	msg = temp;
-}else{
-	//complain, clean up, and return the function
-}
-robotID = atoi(current.c_str());
-
-//find the end of xcord
-end = msg->find('$');
-
-if (end != std::string::npos){
-	//pull out the id and create a temporary string that is the 
-	//current string minus the xCord, delete the current string and 
-	//reassign the pointer to the new string;
-	current = msg->substr(0, end);
-		std::string* temp = new std::string(msg->substr(end+1, std::string::npos));
-		delete msg;
-		msg = temp;
-	}else{
-		//complain, clean up, and return the function
-	}
-
-	x = atoi(current.c_str());
-
-	
-	//find the end of Ycord
-	end = msg->find('$');
-
-	if (end != std::string::npos){
-		//pull out the id and create a temporary string that is the 
-		//current string minus the ycord, delete the current string and 
-		//reassign the pointer to the new string;
-		current = msg->substr(0, end);
-			std::string* temp = new std::string(msg->substr(end+1, std::string::npos));
-			delete msg;
-			msg = temp;
-		}else{
-
-			//since this is the last element if there is no dollar sign, it is the last 
-			//so we set the end flag for the loop and go ahead and delete the msg
-			moreRobots = false;
-			current = msg->substr(0, msg->size() - 1);
-			delete msg;
-		}
-
-		y = atoi(current.c_str());
-
-	
-		//switch to do stuff with the robot data based on weather on not we 
-		//are creating or updating robots
-		Robot* robot_temp;
-		switch(use){
-			case ROBOT_INIT:
-				// create a robot and initialize it from the string
-				// then add it to the list
-				robot_temp = new Robot(connEP, robotID);
-				robot_temp->setXCord(x);
-				robot_temp->setYCord(y);
-				robots->lock();
-				robots->push_back(robot_temp);
-				robots->unlock();
-				break;
-			case ROBOT_UPDATE:
-				Vector_ts<Robot*>::iterator it;
-
-				for(it = robots->begin(); it < robots->end(); ++it){
-					
-					if ((*it)->getEndpoint() == connEP  && (*it)->getRID() == robotID){
-						(*it)->lock();
-						(*it)->setXCord(x);
-						(*it)->setYCord(y);
-						(*it)->lock();
-					}
-				
-				}
-				break;
-		}//switch
-	}//while
-}//function
 
 void RobotHandler::cleanupConn(boost::asio::ip::tcp::endpoint connEP){
 	//get a lock on the robot vector and declare and iterator
