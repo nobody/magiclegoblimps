5b886721fbe0f81e059b02cd3e5a932d04041fda
fedaykin422 <fedaykin422@445d4ad4-0937-11df-b996-818f58f34e26>
2010-03-30 17:37:49 +0000

    adding

---
+++ Server/Controller/Assignment.h
@@ -0,0 +1,37 @@
+/*
+ * Assignments.h
+ *
+ * Modified on:    $Date: $
+ * Last Edited By: $Author: $
+ * Revision:       $Revision: $
+ */
+
+#ifndef ASSIGNMENT_H_
+#define ASSIGNMENT_H_
+
+class Assignment {
+    public:
+        Assignment(Robot* r, int nr, Object* o,int no,  double* d, Qos* q);
+        virtual ~Assignment();
+		map<Robot*, int> calcAssignments();
+
+    private:
+		QOS* quality;
+		Robot* robots;
+		int numRobots;
+		Object* objects;
+		int numObjects;
+		double* demand;
+		double gamma;         //Multplier for cost term
+		int robotAssignments[];
+		void initAssignments();
+		int isDone();         //Returns -1 if done, else returns index of next robot to assign
+		
+		
+};
+
+
+#endif /* ASSIGNMENT_H */
+
+
+/* vi: set tabstop=4 expandtab shiftwidth=4 softtabstop=4: */
