c3d00d948e68c435e889773b6bba208918008909
mealey.patrick <mealey.patrick@445d4ad4-0937-11df-b996-818f58f34e26>
2010-04-02 03:42:40 +0000

    [Server] Added a write method to VideoHandler

--- Server/Controller/VideoHandler.h
+++ Server/Controller/VideoHandler.h
@@ -10,6 +10,7 @@
 #define VIDEOHANDLER_H_
 
 #include <iostream>
+#include <deque>
 
 #include <boost/date_time/posix_time/posix_time.hpp>
 #include <boost/lexical_cast.hpp>
@@ -27,12 +28,14 @@ class VideoHandler : public TcpServer::ConnHandler {
         virtual ~VideoHandler();
 
         virtual void onConnect(TcpServer::TcpConnection::pointer tcp_connection);
+        void write(std::string msg);
 
     private:
         void threaded_on_connect(TcpServer::TcpConnection::pointer);
 
         TcpServer::TcpConnection::pointer conn_;
         message read_message_;
+        void internal_write(std::string);
         void write_handler(const boost::system::error_code& error,  std::size_t bytes_transferred);
         void read_handler(const boost::system::error_code& error,  std::size_t bytes_transferred);
         void close();
@@ -41,6 +44,7 @@ class VideoHandler : public TcpServer::ConnHandler {
         
         Vector_ts<Robot*>* robots_;
         Vector_ts<Object*>* objs_;
+        std::deque<std::string> write_queue_;
 };
 
 
