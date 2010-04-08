fea48503e7a9905e743a8cc54231b04f9c2ac3de
jordanbach <jordanbach@445d4ad4-0937-11df-b996-818f58f34e26>
2010-04-08 17:02:36 +0000

    Fixed indentation issue

--- Server/VideoDelivery/archives.py
+++ Server/VideoDelivery/archives.py
@@ -6,29 +6,27 @@ import socket
 import config
 import ffserver
 import settings
+from vidcontrol import change_working_directory
 
 class Archive():
 
-	ARCHIVE_DIR = './archives'
+    def __init__(self):
+        self.filenames = []
+        self.objects = []
+        self.qos = []
+        self.objects_qos = []
 
-	def __init__(self):
-		self.filenames = []
-		self.objects = []
-		self.qos = []
-		self.objects_qos = []
+    def get_Archive(self):
+        self.filenames = os.listdir(settings.ARCHIVE_DIR)
+        for x in self.filenames:
+            self.qos.append(self.filenames[:3])
+        for x in self.filenames:
+            self.objects.append(self.filenames[4:6])
+        for x in range(len(self.filenames)):
+            self.objects_qos[self.objects[x]] = self.qos[x]
 
-	def get_Archive(self):
-		self.filenames = os.listdir(ARCHIVE_DIR)
-		for x in self.filenames:
-			self.qos.append(self.filenames[:3])
-		for x in self.filenames:
-			self.objects.append(self.filenames[4:6])
-		for x in range(len(self.filenames)):
-			self.objects_qos[self.objects[x]] = self.qos[x]
-
-if __name == '__main__':
-	os.chdir(settings.ROOT_DIR)
-	try:
-		ar = Archive()
-		ar.get_Archive()
-		print self.objects_qos
+if __name__ == '__main__':
+    change_working_directory()
+    ar = Archive()
+    ar.get_Archive()
+    print(str(self.objects_qos))
