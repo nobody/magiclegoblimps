f5a97c5eee14d6f9995c159a76ade5cadb746e52
tsheerin <tsheerin@445d4ad4-0937-11df-b996-818f58f34e26>
2010-04-14 06:38:35 +0000

    [robot] Hopefully this doesn't break anything. Initial commit for the path generation code. It's passed simple tests (woot!), needs more thorough testing though.
    Added:
    Path class - maintains the path for a robot
    GridLoc class - x, y coordinate pair for grid locations
    genPath() - generates a Path
    other utility functions for supporting above additions
    
    Notes with this commit:
    All Controller-Server communication has been commented out due to unsolvable linker errors.
    
    Definitely has lots of memory leaks.

---
+++ trunk/Robot/controller/Path.cpp
@@ -0,0 +1,94 @@
+#include <cmath>
+#include "Path.h"
+
+Path::Path(void)
+{
+	metric = 0;
+}
+
+void Path::extend(GridLoc* pt)		{ path.push_back(pt); }
+
+GridLoc* Path::advPath()
+{
+	GridLoc* gl;
+	if(path.front())
+	{
+		gl = path.front();
+		path.erase(path.begin());
+	}
+	return gl;
+}
+
+void Path::setMetric(double newMet)	{ metric = newMet; }
+void Path::clearPath()				{ path.clear(); }
+
+vector<GridLoc*> Path::getPath()	{ return path; }
+GridLoc* Path::getStart()			{ return path.front(); }
+GridLoc* Path::getEnd()				{ return path.back(); }
+int Path::getSize()					{ return (int)path.size(); }
+double Path::getMetric() const		{ return metric; }
+
+Path* Path::copy()
+{
+	Path* cpy = new Path();
+	for(int i = 0; i < path.size(); i++)
+		cpy->extend(new GridLoc(path[i]->getX(), 
+			path[i]->getY()));
+	return cpy;
+}
+
+void Path::calcMetric(GridLoc curr, GridLoc dest)
+{
+	int rLocX = curr.getX();
+	int rLocY = curr.getY();
+
+	int rDestX = dest.getX();
+	int rDestY = dest.getY();
+
+	int pEndX = path.back()->getX();
+	int pEndY = path.back()->getY();
+
+	double distCoveredSqrd = fabs((double)(rLocX - pEndX))
+		+ fabs((double)(rLocY - pEndY));
+	double distRemainingSqrd = fabs((double)(rDestX - pEndX))
+		+ fabs((double)(rDestY - pEndY));
+	metric = (1.0/distCoveredSqrd) + distRemainingSqrd 
+		+ path.size();
+}
+
+//bool operator<(const Path& p1, const Path& p2)
+//{
+//	printf("%f < %f = %d\n", p1.getMetric(), p2.getMetric(), p1.getMetric() < p2.getMetric());
+//	//cout << p1.getMetric() << " < " << p2.getMetric() << " = " << p1.getMetric() < p2.getMetric() << "\n";
+//	return p1.getMetric() < p2.getMetric();
+//}
+//
+//bool operator<=(const Path& p1, const Path& p2)
+//{
+//	printf("%f <= %f = %d\n", p1.getMetric(), p2.getMetric(), p1.getMetric() <= p2.getMetric());
+//	//cout << p1.getMetric() << " < " << p2.getMetric() << " = " << p1.getMetric() < p2.getMetric() << "\n";
+//	return p1.getMetric() <= p2.getMetric();
+//}
+//
+//bool operator>(const Path& p1, const Path& p2)
+//{
+//	printf("%f > %f = %d\n", p1.getMetric(), p2.getMetric(), p1.getMetric() < p2.getMetric());
+//	//cout << p1.getMetric() << " < " << p2.getMetric() << " = " << p1.getMetric() < p2.getMetric() << "\n";
+//	return p1.getMetric() > p2.getMetric();
+//}
+//
+//bool operator>=(const Path& p1, const Path& p2)
+//{
+//	printf("%f >= %f = %d\n", p1.getMetric(), p2.getMetric(), p1.getMetric() >= p2.getMetric());
+//	//cout << p1.getMetric() << " < " << p2.getMetric() << " = " << p1.getMetric() < p2.getMetric() << "\n";
+//	return p1.getMetric() >= p2.getMetric();
+//}
+
+bool Path::operator()(const Path* p1, const Path* p2)
+{
+	return p1->getMetric() > p2->getMetric();
+}
+
+Path::~Path(void)
+{
+}
---
+++ trunk/Robot/controller/Path.h
@@ -0,0 +1,37 @@
+#ifndef PATH_H
+#define PATH_H
+
+#include <vector>
+#include "GridLoc.h"
+//#include "Robot.h"
+
+using namespace std;
+
+class Path
+{
+public:
+	Path(void);
+
+	void extend(GridLoc* pt);
+	GridLoc* advPath(void);
+	void setMetric(double newMet);
+	void clearPath(void);
+
+	vector<GridLoc*> getPath(void);
+	GridLoc* getStart(void);
+	GridLoc* getEnd(void);
+	int getSize(void);
+	double getMetric(void) const;
+	Path* copy(void);
+
+	void calcMetric(GridLoc curr, GridLoc dest);
+
+	bool operator()(const Path* p1, const Path* p2);
+
+	~Path(void);
+private:
+	vector<GridLoc*> path;
+	double metric;
+};
+
+#endif
