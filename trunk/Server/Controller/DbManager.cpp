085c699d4ba53069052c3c5df0ee5bb048acc61c
mealey.patrick <mealey.patrick@445d4ad4-0937-11df-b996-818f58f34e26>
2010-04-21 04:27:40 +0000

    [Server] frakking db

--- trunk/Server/Controller/DbManager.cpp
+++ trunk/Server/Controller/DbManager.cpp
@@ -375,7 +375,7 @@ bool DbManager::updateCameras( Vector_ts<Robot*>* robots) {
                << (*it)->getXCord()
                << "', '"
                << (*it)->getYCord()
-               << "', '";
+               << "', ";
             float max_qual = -1;
             int max_qual_id = -1;
             std::map<int, float>* list = (*it)->list;
@@ -398,11 +398,11 @@ bool DbManager::updateCameras( Vector_ts<Robot*>* robots) {
                 //std::cout << "[db] Succeeded query: " << cmd << "\n";
             }
             if (max_qual_id > -1)
-                ss << max_qual_id;
+                ss << << "'" << max_qual_id << "'";
             else 
                 ss << "NULL";
 
-            ss << "', '"
+            ss << ", '"
                << (*it)->getDir()
                << "')";
             cmd = ss.str();
@@ -410,13 +410,13 @@ bool DbManager::updateCameras( Vector_ts<Robot*>* robots) {
             try {
                 stmt->execute(cmd);
             } catch(...){
-                //std::cout << "[db] Failed query: " << cmd << "\n";
+                std::cout << "[db] Failed query: " << cmd << "\n";
                 
                 (*it)->unlock();
                 ret = false;
                 continue;
             }
-            //std::cout << "[db] Succeeded query: " << cmd << "\n";
+            std::cout << "[db] Succeeded query: " << cmd << "\n";
             (*it)->unlock();
         }
     }
