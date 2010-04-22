68347057d8ab5c20a80b0b2fa64636a225d511f1
mealey.patrick <mealey.patrick@445d4ad4-0937-11df-b996-818f58f34e26>
2010-04-22 01:14:03 +0000

    [Server] demand normalization tweak

--- trunk/Server/Controller/DbManager.cpp
+++ trunk/Server/Controller/DbManager.cpp
@@ -74,7 +74,9 @@ bool DbManager::normalize(demand_t*& d ) {
     }
 
     for (it = d->begin(); it != d->end(); ++it) {
-        if (it->second > 0.00000001)
+        if (it->second < 0.00000001)
+            it->second = 0;
+        else
             it->second = it->second / total_demand;
         std::cout << "[db] noramlized demand for " << it->first << ":" << it->second << "\n"; 
     }
